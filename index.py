a2eecadff15ca2aecab1745131d63e048738cd32
Romeo <kilazlove@gmail.com>
2021-11-13 21:10:50 +0800

    paste the assignment 2A for editing. Then apply the function

--- index.py
+++ index.py
@@ -1,2 +1,18 @@
 # Assignment 3B
 # Redo the programs on assignment2 but now with functions
+
+apples_quantity = int(input("How many apples you want to buy: "))
+orange_quantity = int(input("How many oranges you want to buy: "))
+
+apple_Price = 20
+orange_Price = 25
+
+apple_Amount = 0
+orange_Amount = 0
+
+if apples_quantity < 0 or orange_quantity < 0:
+    print("Quantity cannot be less than zero")
+else :
+    apple_Amount = apples_quantity * apple_Price
+    orange_Amount = orange_quantity * orange_Price
+    print("The total amount is: " + str(apple_Amount + orange_Amount))
